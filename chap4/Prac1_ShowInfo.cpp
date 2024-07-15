b11c295ac64a9e17a0124186240edec3ac1d1663
moija <91176482+jianwuwo@users.noreply.github.com>
2024-07-15 12:45:55 +0800

    prac

---
+++ chap4/Prac1_ShowInfo.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+int main() {
+    using namespace std;
+
+    char firstName[20], lastName[20];
+    char grade;
+    int age;
+
+    cout << "What is your first name? ";
+    cin.getline(firstName, 20);
+    cout << "What is your last name?";
+    cin.getline(lastName, 20);
+    cout << "What letter grade do you deserve?";
+    cin >> grade;
+    cout << "What is your age?";
+    cin >> age;
+
+    cout << "Name: " << lastName << "." << firstName << endl;
+    cout << "Grade: " << char(grade + 1) << endl;
+    cout << "Age: " << age << endl;
+
+
+    return 0;
+}
--- chap4/Test6_Address.cpp
+++ chap4/Test6_Address.cpp
@@ -29,5 +29,42 @@ int main6() {
     cout << "size of pt = " << sizeof(pt) << " size of *pt = " << sizeof(*pt) << endl;
     cout << "size of pd = " << sizeof(pd) << " size of *pd = " << sizeof(*pd) << endl;
 
+    delete pt;
+    delete pd;
+
+    double* p3 = new double[3];
+    p3[0] = 0.2;
+    p3[1] = 0.5;
+    p3[2] = 0.8;
+    cout << "p3[1] = " << p3[1] << endl;
+    p3 += 1;
+    cout << "NEW NEW p3[0] = " << p3[0] << endl << "NEW NEW p3[1] = " << p3[1] << endl;
+    p3 -= 1;
+
+    delete[]p3;
+
+    double wages[3] = { 10000.0,20000.0,30000.0 };
+    short stacks[3]{ 3,2,1 };
+    
+    double* pw = wages;
+    short* ps = &stacks[1];
+
+    cout << "pw = " << pw << " *pw = " << *pw << endl;
+    pw += 1;
+    cout << "ADD 1: pw = " << pw << " *pw = " << *pw << endl << endl;
+
+    cout << "ps = " << ps << " *ps = " << *ps << endl;
+    ps += 1;
+    cout << "ADD 1: ps = " << ps << " *ps = " << *ps << endl << endl;
+
+    cout << "access 2 elements with array notation\n";
+    cout << "stacks[0] = " << stacks[0] << " stacks[1] = " << stacks[1] << endl;
+    cout << "access 2 elements with pointer notation\n";
+    cout << "*stacks = " << *stacks << " *(stacks+1) = " << *(stacks + 1) << endl << endl;
+
+    cout << "size of wages array = " << sizeof(wages) << endl;
+    cout << "size of pw pointer = " << sizeof(pw) << endl << endl;
+
+
     return 0;
 }
